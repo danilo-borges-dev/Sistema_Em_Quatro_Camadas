9442e8f1291f9ba9431cbf09d0a92ced6afa0630
danilo borges <danilosilva2017@hotmail.com>
2023-01-13 06:07:57 -0300

    Adicionado as 4 camdas fisicas e definido o Projeto de inicializacao

---
+++ Sistema/Sistema.Dados/Conexao.cs
@@ -0,0 +1,20 @@
+﻿namespace Sistema.Dados
+{
+    internal class Conexao
+    {
+        private string _base;
+        private string _servidor;
+        private string _usuario;
+        private string _chave;
+        private bool _seguranca;
+        private static Conexao _con = null;
+        private Conexao()
+        {
+            _base = "DbSistema";
+            _servidor = @"DESKTOP-5HC68GT\SQLEXPRESS";
+            _usuario = "sa";
+            _chave = "123456";
+            _seguranca = seguranca;
+        }
+    }
+}
